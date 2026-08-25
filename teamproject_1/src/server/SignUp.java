7a4de1e2e2578d5e604bf015171ee5c02ab978a7
DevDash <jeonghs1570@gmail.com>
2026-08-25 23:06:42 +0900

    SignUp.java

--- teamproject_1/src/server/SignUp.java
+++ teamproject_1/src/server/SignUp.java
@@ -19,25 +19,66 @@ public class SignUp implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String html = Files.readString(Path.of("src/html/signup.html"));
-        SimpleHttpServer.sendResponse(exchange, 200, SimpleHttpServer.TYPE_HTML, html);
-
         String method = exchange.getRequestMethod();
-        if(!method.equals("POST")) {
+        // GET 요청
+        if (method.equals("GET")) {
+            handleGet(exchange);
+        } else if (method.equals("POST")) {
             handlePost(exchange);
-        }
-        else{
-            exchange.getResponseHeaders().set("Allow","POST");
+        } else {
+            exchange.getResponseHeaders()
+                    .set("Allow", "GET, POST");
             SimpleHttpServer.sendResponse(exchange, 405,
                     SimpleHttpServer.TYPE_TEXT, "지원하지 않는 메서드 입니다");
         }
     }
 
-    private void handlePost(HttpExchange exchange) throws IOException{
+    // GET 시 - 회원가입 페이지
+    private void handleGet(HttpExchange exchange) throws IOException {
+        String html = Files.readString(Path.of("src/html/signup.html"));
+        SimpleHttpServer.sendResponse(exchange, 200,
+                SimpleHttpServer.TYPE_TEXT, html);
+
+    }
+
+    // POST 시 - 회원가입 전송
+    private void handlePost(HttpExchange exchange) throws IOException {
+        // Http 요청 바디 읽기
         String requestBody = SimpleHttpServer.readRequestBody(exchange);
         System.out.println("회원가입 요청 : " + requestBody);
 
+        // form 데이터
+        Map<String, String> formData = parseFormData(requestBody);
 
+        // html에서 설정한 값 읽기
+        String name = formData.get("name");
+        String id = formData.get("id");
+        String pw = formData.get("pw");
+        String email = formData.get("email");
 
+        // 검증
+        if (name == null || id == null || pw == null || email == null
+                || name.isBlank() || id.isBlank() || pw.isBlank() || email.isBlank()) {
+
+            SimpleHttpServer.sendResponse(exchange, 400,
+                    SimpleHttpServer.TYPE_TEXT, "모두 입력해주세요");
+
+            return;
+        }
+
+        // 아이디 중복 검사
+        if(UserApiHandler.existsId(id)){
+            SimpleHttpServer.sendResponse(exchange, 409, SimpleHttpServer.TYPE_TEXT, "이미 존재하는 아이디입니다.");
+            return;
+        }
+
+        // 리스트에 정보 저장
+        UserApiHandler.addUser(user);
+        System.out.println("회원가입 완료" + user);
+
+        // 회원가입 완료하면 로그인 페이지로 이동하기
+        exchange.getResponseHeaders().set("Location", "/api/login");
+        exchange.sendResponseHeaders(300, -1);
+        exchange.close();
     }
 }
