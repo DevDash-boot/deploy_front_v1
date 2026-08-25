510d8e76d2c181bc0b8bcfd72253de673ab0628f
DevDash <jeonghs1570@gmail.com>
2026-08-25 23:12:28 +0900

    Login.java

--- teamproject_1/src/server/Login.java
+++ teamproject_1/src/server/Login.java
@@ -6,23 +6,73 @@ import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        // GET 요청
+        if (method.equals("GET")) {
+            handleGet(exchange);
+        } else if (method.equals("POST")) {
+            handlePost(exchange);
+        } else {
+            exchange.getResponseHeaders()
+                    .set("Allow", "GET, POST");
+            SimpleHttpServer.sendResponse(exchange, 405,
+                    SimpleHttpServer.TYPE_TEXT, "지원하지 않는 메서드 입니다");
+        }
+    }
 
+    // GET 시 - 로그인  페이지
+    private void handleGet(HttpExchange exchange) throws IOException {
         String html = Files.readString(Path.of("src/html/login.html"));
+        SimpleHttpServer.sendResponse(exchange, 200,
+                SimpleHttpServer.TYPE_TEXT, html);
 
-        SimpleHttpServer.sendResponse(exchange, 200, SimpleHttpServer.TYPE_HTML, html);
-        // 로그인에서 필요한 기능
-        // 1. 회원가입에 저장된 list 정보 들고 오기
-        // get으로 SimpleHttpServer의 list를 가져오고
-        // 2. 아이디 유무
-        // 만약 exchange.getRequestBody();에서 id가 ~~라면 확인
-        // 3. 비밀번호 유무
-        // 만약 exchange.getRequestBody();에서 pw가 ~~라면 확인
-        // 4. 아이디 - 비밀번호가 일치하는지
-        // 5. 틀렸을 때 에러 메시지 출력, 일치하면 로그인 되어 메인 홈으로 이동
+    }
+
+    // POST 시 - 로그인 전송
+    private void handlePost(HttpExchange exchange) throws IOException {
+        // Http 요청 바디 읽기
+        String requestBody = SimpleHttpServer.readRequestBody(exchange);
+        System.out.println("로그인 : " + requestBody);
+
+        // form 데이터
+        Map<String, String> formData = parseFormData(requestBody);
+
+        // html에서 설정한 값 읽기
+        // 아이디와 비밀번호 가져오기
+        String id = formData.get("id");
+        String pw = formData.get("pw");
+
+        // 검증
+        if (id == null || pw == null|| id.isBlank() || pw.isBlank()) {
+            SimpleHttpServer.sendResponse(exchange,400,SimpleHttpServer.TYPE_TEXT,"아이디와 비밀번호를 입력해주세요.");
+            return;
+        }
+
+        // 아이디 중복 검사
+        if(UserApiHandler.existsId(id)){
+            SimpleHttpServer.sendResponse(exchange, 409, SimpleHttpServer.TYPE_TEXT, "이미 존재하는 아이디입니다.");
+            return;
+        }
+
+        // 로그인 성공 시
+        System.out.println("로그인 성공");
+
+        // 로그인 실패 시
+        if(loginUser ==null){
+            SimpleHttpServer.sendResponse(exchange, 400, SimpleHttpServer.TYPE_TEXT, "로그인 실패");
+            return;
+        }
+
+        // 로그인 성공하면 8080으로 이동
+        exchange.getResponseHeaders().set("Location", "/");
+        exchange.sendResponseHeaders(300, -1);
+        exchange.close();
     }
 }
